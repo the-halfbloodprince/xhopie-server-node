ade8ff1ece1e98d39c9f9e8618402320c0c2e66f
Manish Kumar Das <ammanishkumardas@gmail.com>
2024-01-03 05:22:30 +0530

    [Feature][Store] Added initializeFeatures in the server and refactored server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,19 @@
 import express, {Express, Request, Response} from 'express'
 import { APP_CONFIG } from './config'
+import initializeFeatures from './init/features'
 
 const app: Express = express()
 
 app.get('/', (req: Request, res: Response) => {
-	return res.send('<h1 style="font-size: 10rem">Chipi Chipi Chapa Chapa<h1>')
+	return res.send(`
+		<h1 style="font-size: 10rem">
+			Chipi Chipi Chapa Chapa
+		<h1>
+	`)
 })
 
+initializeFeatures(app)
+
 app.listen(APP_CONFIG.port, () => {
-	console.log(`[SERVER INITIALIZED] Server up and running at ${APP_CONFIG.host}:${APP_CONFIG.port}`)
+	console.log(`[SERVER] Server up and running at ${APP_CONFIG.host}:${APP_CONFIG.port}`)
 })
