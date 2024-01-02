922aa1a5d80fb77f253d7eb605c7da0c5b92ad00
Manish Kumar Das <ammanishkumardas@gmail.com>
2024-01-03 02:24:58 +0530

    define env variables types

--- config/env_variables.ts
+++ config/env_variables.ts
@@ -1,4 +1,17 @@
-export type ENV_Variables = {
+// ? Declare all your env variable types here
+
+/* 
+? I generally prefer using types and then later do something like
+? type ProcessEnv = {} & ENV_Variables
+! but that didn't work, so choosing some other method for now 
+*/
+// export type ENV_Variables = {
+//   PORT: string;
+//   EXAMPLE_VARIABLE: string;
+// }
+
+// ? reverting to use interface so that the ProcessEnv interface can automatically inherit it
+export default interface ENV_Variables {
   PORT: string;
   EXAMPLE_VARIABLE: string;
 }
