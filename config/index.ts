3463f0092bfba22a3acdcb21e1cf9f49258f404f
Manish Kumar Das <ammanishkumardas@gmail.com>
2024-01-03 02:40:56 +0530

    [FEATURE] Added automatic and centralized config handling (loading and exporting)

---
+++ config/index.ts
@@ -0,0 +1,14 @@
+import { config as loadEnvVariables } from 'dotenv'
+import type ENV_Variables from './env_variables'
+
+const result = loadEnvVariables()
+
+if (result.error) {
+  throw('[ERROR] Error parsing the env variables')
+}
+
+export const APP_CONFIG = {
+  port: process.env.PORT || 8080,
+  host: process.env.HOST || 'http://localhost',
+  example_variable: process.env.EXAMPLE_VARIABLE
+}
