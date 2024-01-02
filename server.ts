7a6e71eab9909a9de52d7d2528ec921ffa6eeeee
Manish Kumar Das <ammanishkumardas@gmail.com>
2024-01-03 02:41:42 +0530

    [REFACTOR] Added refactors to the server file to accomodate config / env file loading changes

--- server.ts
+++ server.ts
@@ -1,14 +1,12 @@
-import express, {Express, Request, Response} from 'express';
+import express, {Express, Request, Response} from 'express'
+import { APP_CONFIG } from './config'
 
-const app: Express = express();
+const app: Express = express()
 
 app.get('/', (req: Request, res: Response) => {
-	return res.send('<h1 style="font-size: 10rem;">Chipi Chipi Chapa Chapa<h1>');
+	return res.send('<h1 style="font-size: 10rem">Chipi Chipi Chapa Chapa<h1>')
 })
 
-const PORT = process.env.PORT || 8080;
-const HOST = process.env.host || 'http://localhost';
-
-app.listen(PORT, () => {
-	console.log(`[server] Server up and running at ${HOST}:${PORT}`);
+app.listen(APP_CONFIG.port, () => {
+	console.log(`[SERVER INITIALIZED] Server up and running at ${APP_CONFIG.host}:${APP_CONFIG.port}, secret_msg: ${APP_CONFIG.example_variable}`)
 })
